775a3f1b64a18a9dc559e6b14c1b3c75b6562911
Numan Saady <numansaady@gmail.com>
2022-02-21 00:11:29 +0600

    task 3.3 added

--- practice.js
+++ practice.js
@@ -33,3 +33,11 @@ console.log(mp);
   const result = addMultiply(3, 4);
   console.log(result);
 }
+
+// ৩.৩ এইবার তিনটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন ডিক্লেয়ার করো। যেই ফাংশনের কাজ হবে তিনটা প্যারামিটার নিয়ে সেই তিনটা প্যারামিটারকে গুণ করে সেই রেজাল্ট রিটার্ন করবে।
+
+{
+  const multiplied = (num1, num2, num3) => num1 * num2 * num3;
+  const output = multiplied(3, 4, 5);
+  console.log(output);
+}
