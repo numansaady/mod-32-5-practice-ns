91cee9b9158e7e3fff209d46cca25de00c928047
Numan Saady <numansaady@gmail.com>
2022-02-21 00:08:26 +0600

    task two added

--- practice.js
+++ practice.js
@@ -2,3 +2,12 @@
 let fName = "Kolimullah"; //let can not redeclare but can reassign
 fName = "Kamal Uddin"; // let can change the value
 const lName = "Milon"; // const can not redeclare and reassign
+
+// ২. টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং তৈরি করো। সেটার মধ্যে উপরে ডিক্লেয়ার করা ভেরিয়েবল এর মান ডাইনামিক ভাবে বসাও। একইভাবে উপরে একটা অবজেক্ট ডিক্লেয়ার করো। এবং ডাইনামিকভাবে উপরের অবজেক্ট এর দুইটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও।
+
+const person = {
+  firstName: "Solim Uddin",
+  lastName: "Selim",
+  designation: "Member of Perloumant",
+  address: "Chhatak",
+};
