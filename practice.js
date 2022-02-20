d667b425bd20538444f2c2dabc26a8f7037f0788
Numan Saady <numansaady@gmail.com>
2022-02-21 00:09:52 +0600

    task 3.1 added

--- practice.js
+++ practice.js
@@ -11,3 +11,12 @@ const person = {
   designation: "Member of Perloumant",
   address: "Chhatak",
 };
+
+const mp = `${fName} ${lName} is a ${person.designation} of ${person.address} area`;
+console.log(mp);
+
+// ৩.১ একটা প্যারামিটার ওয়ালা arrow ফাংশন ডিক্লেয়ার করো। এবং সেই ফাংশনের কাজ হবে তুমি কোন ইনপুট দিলে সেই ইনপুট সংখ্যাকে ৫ দিয়ে ভাগ করে আউটপুট দিবে।
+{
+  const divideIt = (number) => number / 5;
+  console.log(divideIt(25));
+}
